c0a2d723a2d581a987dfe7691b33903a96385010
ariadnagalindom <ariadnagalindom%40gmail.com>
2024-11-28 14:16:12 -0600

    quedan definidas las fechas de correctiva

--- correctiva.py
+++ correctiva.py
@@ -1,31 +1,52 @@
 from datetime import datetime, timedelta
 import pandas as pd
 import os
+import holidays
+
+mx_holidays = holidays.Mexico()
 
 flps = [26, 28, 1, 3, 4, 8, 12, 13, 16, 18, 21]
 cortes = [3, 5, 8, 10, 11, 15, 19, 20, 23, 25, 28]
 
-def fechas_D1(mes, anio):
 
-    fi = []
-    ff= []
-    ruta_fechas = r"\\172.16.39.32\recepcion\FECHAS"
+def get_corte(flp):
+    """
+    Esta función regresa el corte correspondiente al FLP dado.
+    """
+    condiciones = {1:8, 3:10, 4:11, 8:15, 12:19, 13:20, 16:23, 18:25, 21:28, 26:3, 28:5}
+    return condiciones.get(flp, RuntimeWarning)
+
+def fechas_correctiva(mes, anio):
+    """
+    Esta función genera las fechas de asignación correctiva para el mes y año dados.
+    """   
+    fechas = []
     for flp in flps:
-        if flp != 26 and flp != 28:
-            fi.append((datetime(anio, mes, flp) + timedelta(days=mover_dias_corr(datetime(anio, mes, flp).strftime("%A")))).strftime("%Y-%m-%d"))
-            ff.append((datetime(anio, mes, get_corte(flp))).strftime("%Y-%m-%d"))
-    
-    df_d1 = pd.concat([pd.Series(cortes[2:]), pd.Series(fi), pd.Series(ff)], axis=1)
-    # df_d1.to_csv(os.path.join(ruta_fechas, "FECHAS_D1.txt"), index=False, header=False, sep="\t")
+        fecha_correctiva = datetime(anio, mes, flp)
+        if fecha_correctiva.strftime("%A") == "Sunday":
+            fecha_asignacion = fecha_correctiva + timedelta(days=2)
+        elif fecha_correctiva.strftime("%A") == "Saturday":
+            fecha_asignacion = fecha_correctiva + timedelta(days=3)
+        else:
+            fecha_asignacion = fecha_correctiva + timedelta(days=1)
+
+        if fecha_asignacion.strftime("%A") == "Sunday":
+            fecha_asignacion = fecha_asignacion + timedelta(days=1)
             
-def fechas_D2(mes, anio):
+        if fecha_asignacion.strftime("%A") == "Saturday":
+            fecha_asignacion = fecha_asignacion + timedelta(days=2)
 
-    fi = []
-    ff= []
-    ruta_fechas = r"\\172.16.39.32\recepcion\FECHAS"
-    for flp in flps:
-        if flp != 26 and flp != 28:
-            fi.append((datetime(anio, mes, flp+1) + timedelta(days=mover_dias_corr(datetime(anio, mes, flp).strftime("%A")))).strftime("%Y-%m-%d"))
-            ff.append((datetime(anio, mes, get_corte(flp)) + timedelta(days=1)).strftime("%Y-%m-%d"))
-    df_d2 = pd.concat([pd.Series(cortes[2:]), pd.Series(fi), pd.Series(ff)], axis=1)
-    # df_d2.to_csv(os.path.join(ruta_fechas, "FECHAS_D2.txt"), index=False, header=False, sep="\t")
+        # mover un día si es feriado nacional
+        if fecha_asignacion.strftime("%Y-%m-%d") in mx_holidays:
+            fecha_asignacion = fecha_asignacion + timedelta(days=1)
+
+
+        fecha_corte = datetime(anio, mes, get_corte(flp))
+
+        fechas.append([get_corte(flp=flp), fecha_asignacion, fecha_corte])
+    
+    df = pd.DataFrame(fechas, columns=["CORTE", "Fecha Asignación", "Fecha Fin"])
+    print(df)
+
+fechas_correctiva(12,2024)
+        
